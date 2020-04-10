107502cd96162f0cae983335e4a9c80f691341a8
caleb <caleb.geek.gk@gmail.com>
2020-04-10 14:30:14 +0300

    sytling changes

--- src/estimator.js
+++ src/estimator.js
@@ -6,24 +6,24 @@ const covid19ImpactEstimator = (data) => {
   impact.currentlyInfected = data.reportedCases * 10;
   severeImpact.currentlyInfected = data.reportedCases * 50;
   // Duration in days
-  if(data.periodType === 'days') {
+  if (data.periodType === 'days') {
     impact.infectionsByRequestedTime = impact.currentlyInfected
-    * Math.pow(2,Math.floor(data.timeToElapse / 3));
+    * 2 ** Math.floor(data.timeToElapse / 3);
     severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected
-    * Math.pow(2,Math.floor(data.timeToElapse / 3));
+    * 2 ** Math.floor(data.timeToElapse / 3);
   // Duration in weeks
-  }else if(data.periodType === 'weeks') {
+  } else if (data.periodType === 'weeks') {
     impact.infectionsByRequestedTime = impact.currentlyInfected
-    * Math.pow(2,Math.floor((data.timeToElapse * 7) / 3));
+    * 2 ** Math.floor((data.timeToElapse * 7) / 3);
     severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected
-    * Math.pow(2,Math.floor((data.timeToElapse * 7) / 3));
+    * 2 ** Math.floor((data.timeToElapse * 7) / 3);
   // Duration in months
-  }else{
+  } else {
     impact.infectionsByRequestedTime = impact.currentlyInfected
-    * Math.pow(2,Math.floor((data.timeToElapse * 30) / 3));
+    * 2 ** Math.floor((data.timeToElapse * 30) / 3);
     severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected
-    * Math.pow(2,Math.floor((data.timeToElapse * 30) / 3));
-  };
+    * 2 ** Math.floor((data.timeToElapse * 30) / 3);
+  }
   return { data, impact, severeImpact };
 };
 export default covid19ImpactEstimator;
