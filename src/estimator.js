138950fa739d08f8424651f83695a6c5eef86c9a
caleb <caleb.geek.gk@gmail.com>
2020-04-10 13:31:18 +0300

    Fixing months and weeks error

--- src/estimator.js
+++ src/estimator.js
@@ -1,14 +1,29 @@
+
 const covid19ImpactEstimator = (data) => {
   const impact = {};
   const severeImpact = {};
   // CHALLENGE 1
   impact.currentlyInfected = data.reportedCases * 10;
   severeImpact.currentlyInfected = data.reportedCases * 50;
-  impact.infectionsByRequestedTime = impact.currentlyInfected * 10 * 512;
-  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 50 * 512;
-  impact.severeCasesByRequestedTime = Math.floor(impact.infectionsByRequestedTime * (15 / 100));
-  severeImpact.severeCasesByRequestedTime = Math.floor(severeImpact.infectionsByRequestedTime
-  * (15 / 100));
+  // Duration in days
+  if(data.periodType === 'days') {
+    impact.infectionsByRequestedTime = impact.currentlyInfected
+    * Math.pow(2,Math.floor(data.timeToElapse / 3));
+    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected
+    * Math.pow(2,Math.floor(data.timeToElapse / 3));
+  // Duration in weeks
+  }else if(data.periodType === 'weeks') {
+    impact.infectionsByRequestedTime = impact.currentlyInfected
+    * Math.pow(2,Math.floor((data.timeToElapse * 7) / 3));
+    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected
+    * Math.pow(2,Math.floor((data.timeToElapse * 7) / 3));
+  // Duration in months
+  }else{
+    impact.infectionsByRequestedTime = impact.currentlyInfected
+    * Math.pow(2,Math.floor((data.timeToElapse * 30) / 3));
+    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected
+    * Math.pow(2,Math.floor((data.timeToElapse * 30) / 3));
+  };
   return { data, impact, severeImpact };
 };
 export default covid19ImpactEstimator;
