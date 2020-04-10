a45ea3c6885a3d325a4772d61b0656c11ae62a51
caleb <caleb.geek.gk@gmail.com>
2020-04-10 10:13:09 +0300

    More styling adjustment

--- src/estimator.js
+++ src/estimator.js
@@ -1,18 +1,15 @@
 const covid19ImpactEstimator = (data) => {
-    const impact = {};
-    const severeImpact = {};
-    
-    // CHALLENGE 1
-    impact.currentlyInfected = data.reportedCases*10;
-    severeImpact.currentlyInfected = data.reportedCases*50;
-
-    impact.infectionsByRequestedTime = impact.currentlyInfected*10*512;
-    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected*50*512;
-    
-    impact.severeCasesByRequestedTime = Math.floor(impact.infectionsByRequestedTime*15/100);
-    severeImpact.severeCasesByRequestedTime = Math.floor(severeImpact.infectionsByRequestedTime*15/100);
-
- return {data:data,impact,severeImpact};
+  const impact = {};
+  const severeImpact = {};
+  // CHALLENGE 1
+  impact.currentlyInfected = data.reportedCases * 10;
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * 10 * 512;
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 50 * 512;
+  impact.severeCasesByRequestedTime = Math.floor(impact.infectionsByRequestedTime * (15 / 100));
+  severeImpact.severeCasesByRequestedTime = Math.floor(severeImpact.infectionsByRequestedTime * 
+    (15 / 100));
+return {data:data,impact,severeImpact};
 }
 
 export default covid19ImpactEstimator;
