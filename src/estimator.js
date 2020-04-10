3ddbcf38b564cd7b6628a0e251f0d29dfbd5b5ee
caleb <caleb.geek.gk@gmail.com>
2020-04-10 09:14:31 +0300

    challenge 1 code computations

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,18 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    let impact = {}
+    let severeImpact = {}
+    
+    //CHALLENGE 1
+    impact['currentlyInfected']= data.reportedCases*10
+    severeImpact['currentlyInfected'] = data.reportedCases*50
+
+    impact['infectionsByRequestedTime']=impact.currentlyInfected*10*512
+    severeImpact['infectionsByRequestedTime'] = severeImpact.currentlyInfected*50*512
+    
+    impact['severeCasesByRequestedTime']= Math.floor(impact.infectionsByRequestedTime*15/100)
+    severeImpact['severeCasesByRequestedTime']=  Math.floor(severeImpact.infectionsByRequestedTime*15/100)
+
+ return  {data:data,impact,severeImpact}
+}
 
 export default covid19ImpactEstimator;
