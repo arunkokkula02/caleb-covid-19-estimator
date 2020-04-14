b19b4fd8eb90e2a36213942f5636530cad8f5e23
caleb <caleb.geek.gk@gmail.com>
2020-04-14 22:52:44 +0300

    styling

--- src/logger.js
+++ src/logger.js
@@ -1,23 +1,18 @@
 const fs = require('fs');
 
 const getRequestDurationInseconds = (start) => {
-const NS_PER_SEC = 1e9; 
-const NS_TO_S = 1e6; 
-const diff = process.hrtime(start);
+  const NS_PER_SEC = 1e9;
+  const NS_TO_S = 1e6;
+  const diff = process.hrtime(start);
   return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_S;
 };
 
 const logger = (req, res, next) => {
-  const {method,url} = req;
+  const { method, url } = req;
   const start = process.hrtime();
   const duration = getRequestDurationInseconds(start);
   const log = `${method}\t\t${url}\t\tdone in ${duration.toLocaleString()} ms`;
-  
-  fs.appendFile('logs.log', log + '\n', err => {
-    if (err) {
-      return(err);
-    }
-  });
+  fs.appendFile('logs.log', log + '\n');
   next();
 };
 module.exports = logger;
